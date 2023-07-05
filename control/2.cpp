4ddacaee74167a02fbfac89c72adde1fa9d4965a
smena <sergiosantosmenaq@gmail.com>
2023-07-05 11:33:16 -0500

    2 mejor

--- control/2.cpp
+++ control/2.cpp
@@ -2,9 +2,11 @@
 #include<vector>
 using namespace std;
 
-bool existeIndice(vector<int> & vec, int & indice) {
-    if(vec.size() == 0) return 0;
-    if(vec[indice] == indice) return 1;
+void existeIndice(vector<int> & vec, int & indice) {
+    if(vec.size() == 0) return;
+    if(vec[indice] == indice) {
+        cout<<"Si existe un indice"<<indice<<" tal que X["<<indice<<"] = "<<vec[indice]<<endl;
+    }
     return existeIndice(vec, ++indice);
 }
 
@@ -13,11 +15,14 @@ int main() {
     vector<int> vec = {1,1,2,5,7,8,9};
 
     int indice = 0;    
-    if(existeIndice(vec, indice)) {
+    existeIndice(vec, indice);
+
+    /*
+    if() {
         cout<<"Si existe un indice i tal que X[i]"<<endl;
     } else {
         cout<<"No existe un indice i tal que X[i]"<<endl;
-    }
+    }*/
 
     return 0;
 }
