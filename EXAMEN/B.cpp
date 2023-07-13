18521d1c87b24440e69bd42a5933062a398a3d00
smena <sergiosantosmenaq@gmail.com>
2023-07-13 03:07:21 -0500

    B recursivo hecho

--- EXAMEN/B.cpp
+++ EXAMEN/B.cpp
@@ -1,7 +1,9 @@
-#include<iostream>
-#include<math.h>
-#include<vector>
-#include<iomanip>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <cfloat>
 using namespace std;
 
 struct Point
@@ -9,51 +11,110 @@ struct Point
     int x, y;
 };
 
+bool compareX(Point a, Point b)
+{
+    return a.x < b.x;
+}
 
-float getDistance(Point a, Point b) {
+bool compareY(Point a, Point b)
+{
+    return a.y < b.y;
+}
+
+float getDistance(Point a, Point b)
+{
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
-void minDistance() {
+float stripClosest(vector<Point>& strip, float d)
+{
+    float minDistance = d;
 
+    sort(strip.begin(), strip.end(), compareY);
+
+    for (size_t i = 0; i < strip.size(); ++i)
+    {
+        for (size_t j = i + 1; j < strip.size() && (strip[j].y - strip[i].y) < minDistance; ++j)
+        {
+            float distance = getDistance(strip[i], strip[j]);
+            minDistance = min(minDistance, distance);
+        }
+    }
+
+    return minDistance;
+}
+
+float closestUtil(vector<Point>& points, int left, int right)
+{
+    if (right - left <= 3)
+    {
+        float minDistance = FLT_MAX;
+        for (int i = left; i <= right; ++i)
+        {
+            for (int j = i + 1; j <= right; ++j)
+            {
+                float distance = getDistance(points[i], points[j]);
+                minDistance = min(minDistance, distance);
+            }
+        }
+        return minDistance;
+    }
+
+    int mid = (left + right) / 2;
+    Point midPoint = points[mid];
+
+    float leftDistance = closestUtil(points, left, mid);
+    float rightDistance = closestUtil(points, mid + 1, right);
+    float minDistance = min(leftDistance, rightDistance);
+
+    vector<Point> strip;
+    for (int i = left; i <= right; ++i)
+    {
+        if (abs(points[i].x - midPoint.x) < minDistance)
+        {
+            strip.push_back(points[i]);
+        }
+    }
+
+    float stripDistance = stripClosest(strip, minDistance);
+    return min(minDistance, stripDistance);
+}
+
+float closestPair(vector<Point>& points)
+{
+    sort(points.begin(), points.end(), compareX);
+    return closestUtil(points, 0, points.size() - 1);
+}
+
+int main()
+{
     int n;
-    cin>>n;
-
-    vector<Point> points;
+    cin >> n;
 
     while (n != 0)
     {
-        int x, y;
-        for(int i = 0; i < n; i++) {
-            cin>>x>>y;
-            points.push_back({x,y});
+        vector<Point> points(n);
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> points[i].x >> points[i].y;
         }
 
-        float menor = -1;
-        for(int i = 0; i < points.size(); i++) {
-            for(int j = i + 1; j < points.size(); j++) {
-                float distance = getDistance(points[i], points[j]);
-                if(menor == -1 || distance < menor) {
-                    menor = distance;
-                }
-            }
+        float minDistance = closestPair(points);
+
+        if (minDistance >= 10000)
+        {
+            cout << "INFINITY" << endl;
+        }
+        else
+        {
+            cout << fixed << setprecision(4) << minDistance << endl;
         }
 
-        if(menor >= 10000) {
-            cout<<"INFINITY"<<endl;
-        } else {
-            cout<< fixed << setprecision(4) << menor<<endl;
-        }
-
-        cin>>n;
         points.clear();
+        cin >> n;
     }
     
-}
-
-int main() {
-
-    minDistance();
 
     return 0;
 }
