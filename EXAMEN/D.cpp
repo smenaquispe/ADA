cac1288730781dff81aabc740484086db05ecb07
smena <sergiosantosmenaq@gmail.com>
2023-07-13 04:44:08 -0500

    D hecho

--- EXAMEN/D.cpp
+++ EXAMEN/D.cpp
@@ -1,79 +1,80 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
 
-struct Segment {
-    int left;
-    int right;
+struct Pair
+{
+    int L, R;
 };
 
-bool compareSegments(const Segment& seg1, const Segment& seg2) {
-    return seg1.left < seg2.left;
-}
-
-void printSegments(const std::vector<Segment>& segments) {
-    for (const auto& seg : segments) {
-        std::cout << seg.left << " " << seg.right << std::endl;
-    }
-}
-
-void coverSegment(int M, const std::vector<Segment>& segments) {
-    std::vector<Segment> coveredSegments;
-    int maxRight = 0;
-    int i = 0;
-
-    while (maxRight < M && i < segments.size()) {
-        // Find the segment with the maximum right endpoint
-        int maxRightIndex = -1;
-        for (int j = i; j < segments.size(); j++) {
-            if (segments[j].left > maxRight) {
-                break;
-            }
-            if (segments[j].right > segments[maxRightIndex].right) {
-                maxRightIndex = j;
-            }
-        }
-
-        if (maxRightIndex == -1) {
-            break;
-        }
-
-        coveredSegments.push_back(segments[maxRightIndex]);
-        maxRight = segments[maxRightIndex].right;
-        i = maxRightIndex + 1;
-    }
-
-    if (maxRight >= M) {
-        std::cout << coveredSegments.size() << std::endl;
-        printSegments(coveredSegments);
-    } else {
-        std::cout << "0" << std::endl;
-    }
+bool comparePair(Pair & a, Pair & b) {
+    return a.L < b.L;
 }
 
 int main() {
-    int T;
-    std::cin >> T; // Number of test cases
 
-    while (T--) {
+    int N;
+    cin>>N;
+
+    while (N--)
+    {
+        
         int M;
-        std::cin >> M; // Segment [0, M]
+        cin>>M; // es un segmento de 0 -> M en x
 
-        std::vector<Segment> segments;
-        int left, right;
-        while (std::cin >> left >> right && !(left == 0 && right == 0)) {
-            segments.push_back({left, right});
+        int L = 1, R = 1;
+
+        vector<Pair> pairs;
+
+        while (true)
+        {
+            cin>>L>>R;
+
+            if(L == 0 && R == 0) break;
+
+            pairs.push_back({L, R});
         }
 
-        std::sort(segments.begin(), segments.end(), compareSegments);
+        sort(pairs.begin(), pairs.end(), comparePair);
 
-        coverSegment(M, segments);
 
-        // Print a blank line between consecutive test cases
-        if (T > 0) {
-            std::cout << std::endl;
+        int minSegments = 0;
+        int currR = 0;
+        int maxR = 0;
+        int i = 0;
+
+        while (i < pairs.size() && currR < M)
+        {
+            if(pairs[i].L <= currR) {
+                if(pairs[i].R > maxR) {
+                    maxR = pairs[i].R;
+                    currR = maxR;
+                    minSegments++;
+                }
+                i++;
+            } else {
+                if(maxR > currR) {
+                    currR = maxR;
+                    minSegments++;
+                } else {
+                    break;
+                }
+            }
         }
+        
+
+        if(currR >= M) {
+            cout<<minSegments<<endl;
+            for(int j = i - 1; j < minSegments + 1; j++) {
+                cout<<pairs[j].L<<" "<<pairs[j].R<<endl;
+            }
+        } else {
+            cout<<0<<endl;
+        }
+
     }
+    
 
     return 0;
 }
